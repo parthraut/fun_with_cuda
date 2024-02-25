c843ee944ed6ce08eabf7e810b4b1b6c068b77e4
Parth Raut <praut@umich.edu>
2024-02-24 19:54:21 -0500

    fixed shell script

--- matrix_multiplication/driver.sh
+++ matrix_multiplication/driver.sh
@@ -1,10 +1,17 @@
-# bin/bash
+#!/bin/bash
 
-# compile using nvcc
-nvcc -o matrix_multiplication matrix_multiplication.cu
+# Compile CUDA program using nvcc
+if ! nvcc -o matrix_multiplication matrix_multiplication.cu; then
+    echo "Error: Compilation failed"
+    exit 1
+fi
 
-# run the program with values [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-for i in 1 2 4 8 16 32 64 128 256 512 1024
+# Run the program with values [1, 2, 4, ..., 1024]
+for i in {1..10} 1024
 do
-    ./matrix_multiplication $i
+    echo "Running with value: $i"
+    if ! ./matrix_multiplication $i; then
+        echo "Error: Execution failed for value $i"
+        exit 1
+    fi
 done
